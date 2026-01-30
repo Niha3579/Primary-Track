8596cc061560851a4ac4c4da5d6f61e85e378bcd
Niha3579 <147632741+Niha3579@users.noreply.github.com>
2026-01-30 14:48:25 +0530

    Delete DAY 4 directory

--- DAY 4/3logfile_extract.py
+++
@@ -1,7 +0,0 @@
-with open("sample_logs.log","r") as log:
-    lines=log.readlines()
-
-for line in lines:
-    if "ERROR" in line:
-        with open("error_extract.txt","w") as file:
-            file.write(line)
--- DAY 4/exception_handling.py
+++
@@ -1,6 +0,0 @@
-print("Step 1")
-a =10
-b=20
-result=a/b
-print("step 2")
-print("Result is:", result)
